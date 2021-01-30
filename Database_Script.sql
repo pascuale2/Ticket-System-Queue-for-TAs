7ec6c00cb5d5974aba7f3209b78650ba596aefe0
Erwin Pascual <56744638+pascuale2@users.noreply.github.com>
2021-01-30 11:13:32 -0700

    Update Database_Script.sql

--- Database_Script.sql
+++ Database_Script.sql
@@ -19,6 +19,7 @@ CREATE TABLE Discipline(
 
 CREATE TABLE Teacher(
   teacher_id int NOT NULL PRIMARY KEY,
+  email varchar(255),
   name varchar(255),
   discipline_id int FOREIGN KEY REFERENCES Discipline(discipline_id),
   available bit
@@ -26,6 +27,7 @@ CREATE TABLE Teacher(
 
 CREATE TABLE Student(
   student_id int NOT NULL PRIMARY KEY,
+  email varchar(255),
   name varchar(255),
   biography varchar(255),
   discipline_id int FOREIGN KEY REFERENCES Discipline(discipline_id)
