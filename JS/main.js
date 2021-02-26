d9781c4c50129553c6788364359517b7c25cbc1b
pascuale2 <epas97@hotmail.com>
2021-02-26 11:52:14 -0700

    Fixed JS not loading CSS

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,92 @@
 const http = require('http');
 const fs = require('fs');
+const mysql = require('mysql');
+const path = require('path');
+const port = process.env.PORT || 3200;
 
-var server = http.createServer(function(req, res) {
+/*
+ * Creates a connection to the database
+ * @params req: http request, res: http result
+ * @return null on success
+ */
+function configDatabase(req, res) {
 
-    console.log('request to '+req.url + ' was made');
+  var connection = mysql.createConnection({
+    host: "mysql.mapledonut.ca",
+    user: "mapledonutca1",
+    password: "UQ8P2mdX",
+    database: "mapledonut_ca"
+  });
 
-   req.on('error', (err) => {
-        console.error(err);
-        req.statusCode = 400;
-        req.end();
-    });
-    res.on('error', (err) => {
-        console.error(err);
-    });
-    //res.write(JSON.stringify(["dr john - Chem", "Dr. Borh - Bio", "Dr Sahm - Physics"]));
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/HTML/index.html', 'utf8');
-    myReadStream.pipe(res);
+  connection.connect(function(err) {
+    if(err){
+      return console.log("error" + err.message);              // connection failed
+    }else{
+      console.log("connected to mapledonut_ca");                // connection success
+      //let insertQuery = 'INSERT INTO ?? (??,??,??,??) VALUES (?,?,?,?)';
+      //let query = mysql.format(insertQuery,["Student","student_id","email","biography",
+    //  "discipline_id", 100, "test@gmail.com", "test", 101]);
+      let query = 'SELECT * FROM Student';
+      // Select all records from teacher table
+      connection.query(query, (err, result) => {
+        if(err){                                               // query failed
+          console.log(err);
+        }else{                                                  // query success
+          return console.log("result: " + result[0].student_id);
+        }
+      });
+
+    }
+  });
+
+
+}
+
+/*
+ * Create a http server running on port 3200
+*/
+const server = http.createServer(function(req, res) {
+  let filePath = path.join(
+    __dirname,
+    "..",
+    req.url === "/" ? "/HTML/login.html": req.url
+  );
+  configDatabase(req, res);                             // Configure the database
+  console.log('request to '+ req.url + ' was made');
+
+  //res.write(JSON.stringify(["dr john - Chem", "Dr. Borh - Bio", "Dr Sahm - Physics"]));
+  let extName = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extName) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  console.log(`File Path: ${filePath}`);
+  console.log(`Content Type: ${contentType}`);
+
+  res.writeHead(200, {'Content-Type': contentType});
+  var myReadStream = fs.createReadStream(filePath);
+  myReadStream.pipe(res);
 });
-
-server.listen(3200);
-console.log('listening on port 3200...\n');
+server.listen(port, (err) =>{
+  if (err) {
+    console.log(`Error: ${err}`);
+  } else {
+    console.log(`Server listening at port ${port}...`);
+  }
+});
