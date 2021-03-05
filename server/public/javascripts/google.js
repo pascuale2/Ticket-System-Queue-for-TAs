9c234aabe10a020f519c30ebccd0b21c75049ab9
Seth Thompson <Thompsonseth>
2021-03-04 17:07:21 -0700

    added logout functionality to home,courses,questions,professors,discussions

--- server/public/javascripts/google.js
+++ server/public/javascripts/google.js
@@ -17,4 +17,10 @@ function initSignIn() {
         hosted_domain: 'mymacewan.ca',
       });
     });
-  };
+  }
+/*function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}*/
