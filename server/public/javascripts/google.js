d1213b59e15a169daad5d019a3c403975b580091
dylanollikka <43556880+dylanollikka@users.noreply.github.com>
2021-03-04 18:08:14 -0700

    Added ability to view professors

---
+++ server/public/javascripts/google.js
@@ -0,0 +1,20 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    window.location.replace("home");
+  }
+function initSignIn() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+        client_id: '798834471674-2hla9ttcnoausu8a6e2gsoo4j0f70v7u.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        ux_mode: 'redirect',
+        redirect_uri: 'http://localhost:3000/home',
+        hosted_domain: 'mymacewan.ca',
+      });
+    });
+  };
---
+++ server/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var db = require('./sql');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+router.get('/discussions', function(req, res, next) {
+  res.render('discussions');
+});
+router.get('/professors', function(req, res, next) {
+  var connection = db.configDatabase(req, res);
+
+  db.obtainAllProfessors(connection, function(result) {
+    res.render('professors', {data: JSON.stringify(result)});
+  });
+
+});
+router.get('/settings', function(req, res, next) {
+  res.render('settings');
+});
+router.get('/questions', function(req, res, next) {
+  res.render('questions');
+});
+router.get('/courses', function(req, res, next) {
+// student_id is in /public/google.js
+  var connection = db.configDatabase(req, res);
+  db.obtainAllCourses(connection, function(result) {
+      res.render('courses', {data: JSON.stringify(result)});
+  });
+});
+router.get('/courses', function(req, res, next) {
+  res.render('courses');
+});
+router.get('/question_success', function(req, res, next) {
+  res.render('question_success');
+});
+router.get('/question_ask', function(req, res, next) {
+  res.render('question_ask');
+});
+router.get('/settings_edit', function(req, res, next) {
+  res.render('settings_edit');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+module.exports = router;
