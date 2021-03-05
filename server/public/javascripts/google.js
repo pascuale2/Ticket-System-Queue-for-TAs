4e2392cce69874067aad35d52e9c43e1355c3337
pascuale2 <epas97@hotmail.com>
2021-03-05 16:08:31 -0700

    Added Additional Content to Professors Page

--- server/public/javascripts/google.js
+++ server/public/javascripts/google.js
@@ -17,7 +17,8 @@ function initSignIn() {
         hosted_domain: 'mymacewan.ca',
       });
     });
-  }
+  };
+
 /*function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
