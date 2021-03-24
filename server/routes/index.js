c244a2a741787976b6bc27ba9265dfa51ea5f1f0
dylanollikka <43556880+dylanollikka@users.noreply.github.com>
2021-03-23 21:04:43 -0600

    post request to question_asked route created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./sql');
+
 var courses_;
 var connection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   connection = db.configDatabase(req, res);
   res.render('login');
 });
@@ -37,20 +39,44 @@ router.get('/professors', function(req, res, next) {
 router.get('/settings', function(req, res, next) {
   res.render('settings');
 });
+
+router.post('/questions', function(req, res, next) {
+    var largestid;
+    db.getQuestionID(connection, function(result) {
+      largestid = result;
+      console.log("\n LARGEST ID IS: ",largestid,"\n");
+      db.askQuestion(connection, req.body.question_ask, 100, largestid, function(result) {
+        console.log("result is ",result);
+      res.render('questions_asked');
+    });
+  });
+
+
+});
 router.get('/questions', function(req, res, next) {
   db.obtainQuestions(connection, function(result) {
     res.render('questions', {data: result});
   });
-
 });
+
 router.get('/courses', function(req, res, next) {
   // student_id is in /public/google.js
     db.obtainAllCourses(connection, function(result) {
         res.render('courses', {data: result});
     });
 });
+
 router.get('/courses', function(req, res, next) {
   res.render('courses');
+});
+router.post('/course_search', function(req, res, next) {
+  db.searchCourses(connection, req.body.course, function(result){
+    console.log("result is ",result);
+    res.render('course_search', {data: result});
+  });
+});
+router.get('/course_search', function(req, res, next) {
+  res.render('course_search')
 });
 router.get('/question_success', function(req, res, next) {
   res.render('question_success');
@@ -58,6 +84,14 @@ router.get('/question_success', function(req, res, next) {
 router.get('/question_ask', function(req, res, next) {
   res.render('question_ask');
 });
+/*
+router.post('/questions_search', function(req, res, next) {                    // For a question search
+  console.log("result is: ",req.body.question);
+  db.searchQuestions(connection, req.body.question, function(result) {
+    res.render('questions_search', {data: result});
+  });
+});
+*/
 router.get('/questions_search', function(req, res, next) {
   res.render('questions_search');
 });
--- server/routes/sql.js
+++ server/routes/sql.js
@@ -92,7 +92,136 @@ function searchProfessor(connection, input, callback) {
     }
   });
 }
+/*
+function searchQuestions(connection, input, callback) {
+  let replacement = `'%${input}%'`;
+  let query = 'SELECT * FROM Question WHERE question_string like '+replacement;
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log("CANNOT execute search", err);
+    } else {
+      result = JSON.parse(JSON.stringify(result));
+      callback(result);
+    }
+  });
+}*/
+
+
+function searchCourses(connection, course, callback) {
+  let replacement = `'%${course}%'`;
+  let query = 'SELECT * FROM Course WHERE course_name like '+replacement;
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log("CANNOT execute search", err);
+    } else {
+      result = JSON.parse(JSON.stringify(result));
+      callback(result);
+    }
+  });
+}
+
+function getQuestionID(connection, callback){
+ let query = 'SELECT MAX(question_id) AS largestId FROM Question WHERE 1=1';
+  connection.query(query, (err, result) => {
+  if (err) {
+  console.log("Cannot find max question_id");
+  } else {
+  result = JSON.parse(JSON.stringify(result[0])).largestId;
+  callback(result);
+  }
+  });
+}
+
+
+function askQuestion(connection, question_string, stu_id, qid, callback) {
+
+  qid +=1;
+  console.log('quid is ',qid);
+  var status = 0;
+
+  let query = 'INSERT INTO Question(question_id, question_string, question_status, student_id) VALUES (?, ?, ?, ?)';
+  connection.query(query, [qid, question_string, status, stu_id],(err, result) => {
+    if (err) {
+      console.log("CANNOT insert into question", err);
+    } else {
+      result = JSON.parse(JSON.stringify(result));
+      callback(result);
+    }
+  });
+}
+/*
+function getQueueID(connection){
+ let query = 'SELECT MAX(queue_id) FROM Queue WHERE 1=1';
+  connection.query(query, (err, result) => {
+  if (err {
+  console.log("Cannot find max queue_id");
+  } else {
+  return result;
+});
+}
+
+function getDiscipline(connection, teach_id) {
+  let query = 'SELECT discipline_id FROM discipline, Teacher WHERE Teacher.teacher_id = teach_id';
+  connection.query(query, (err, result) => {
+  if (err) {
+    console.log("CANNOT insert into question", err);
+  } else {
+    result = JSON.parse(JSON.stringify(result));
+    return result;
+  }
+  });
+}
+
+function obtainQuestionID(connection, question_str) {
+  let query = 'SELECT question_id FROM Question WHERE \
+  question_string = question_str';
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log("CANNOT find question_id", err);
+    }else {
+      result = JSON.parse(JSON.stringify(result));
+      return result;
+    }
+  });
+}
+
+function obtainSessionID(connection, teach_id) {
+let query = 'SELECT session_id FROM Session, Teacher WHERE \
+Teacher.teacher_id = teach_id AND Teacher.teacher_id = Session.teacher_id';
+connection.query(query, (err, result) => {
+  if (err) {
+    console.log("CANNOT find question_id", err);
+  }else {
+    result = JSON.parse(JSON.stringify(result));
+    return result;
+  }
+});
+}
+
+
+function insertIntoQueue(connection, stu_id, teach_id, question_str, callback) {
+  que_id = getQueueID(connection);
+  que_id +=1;
+
+  discipline = getDiscipline(connection, teach_id);
+  question = obtainQuestionID(connection, question_str);
+  session = obtainSessionID(connection, teach_id);
+
+  let query = 'INSERT INTO QUEUE(queue_id,	answer_ids,	question_ids,	student_ids,
+  	teacher_id,	discipline_id,	question_id,	session_id) VALUES(que_id, 'Not answered',
+  stu_id, teach_id, discipline, question, session)';
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log("CANNOT insert into queue", err);
+    } else {
+      result = JSON.parse(JSON.stringify(result));
+      callback(result);
+    }
+  });
+}
+
+*/
 
 
 module.exports = {configDatabase, obtainAllCourses, obtainAllProfessors, obtainTeaches,
-obtainQuestions, searchProfessor};
+obtainQuestions, searchProfessor, searchCourses, getQuestionID, askQuestion};
